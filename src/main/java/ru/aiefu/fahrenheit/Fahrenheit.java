d9da7a954d29b0d3586b4b80f98c0555343a6923
CmdrJane <48736985+CmdrJane@users.noreply.github.com>
2021-04-07 04:34:35 +0300

    init

--- src/main/java/ru/aiefu/fahrenheit/Fahrenheit.java
+++ src/main/java/ru/aiefu/fahrenheit/Fahrenheit.java
@@ -3,12 +3,10 @@ package ru.aiefu.fahrenheit;
 import net.fabricmc.api.ModInitializer;
 
 public class Fahrenheit implements ModInitializer {
+	public static final String MOD_ID = "fahrenheit";
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		System.out.println("Hello Fabric world!");
 	}
 }
